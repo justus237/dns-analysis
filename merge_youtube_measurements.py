4297d03251c17af690f0842c80bfd37af24ac042
justus237 <62303964+justus237@users.noreply.github.com>
2022-03-11 19:09:59 +0100

    update script

--- merge_youtube_measurements.py
+++ merge_youtube_measurements.py
@@ -3,8 +3,10 @@ import sqlite3
 
 tables = ['lookups', 'qlogs', 'page_resources', 'nerd_stats', 'iframe_api', 'measurements']
 dbs = ['web-performance-youtube-afsouth.db', 'web-performance-youtube-apnorth.db', 'web-performance-youtube-apsouth.db', 'web-performance-youtube-eucentral.db', 'web-performance-youtube-saeast.db', 'web-performance-youtube-useast.db', 'web-performance-youtube-uswest.db']
+merged_conn = sqlite3.connect('web-performance-youtube-merged.db')
 for db in dbs:
 	conn = sqlite3.connect(db)
 	for table in tables:
-		df_tmp = pd.read_sql_query("SELECT * from "+table, conn)
-		df_tmp.to_sql(name=table, con=conn, if_exists='append', index=False)
+		pd.read_sql_query("SELECT * from "+table, conn).to_sql(name=table, con=merged_conn, if_exists='append', index=False)
+	conn.close()
+merged_conn.close()
